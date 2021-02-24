eb0f342f8731fa7eacb2d71753563013c795e6a8
sohelrana76 <msrana3876@gmail.com>
2021-02-24 20:52:44 +0600

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 
 
+const searchFood = () => {
+    const meals = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meals}`
+    // load data
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayFood(data.meals))
+}
 
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-.then(response => response.json())
-.then(data => )
+// display food
+const displayFood = foods => {
+    const foodContainer = document.getElementById('food-container')
+    foods.forEach(food => {
+        console.log(food);
+        const foodDiv = document.createElement('div');
+        foodDiv.className = 'card card-width';
+        foodDiv.innerHTML = `
+        <img src="${food.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 onclick="getInstruction('${food.strInstructions}')" class="card-title">${food.strMeal}</h5>
+        </div>
+        
+        `;
+        foodContainer.appendChild(foodDiv);
+    })
+}
+
+// get instructions
+const getInstruction = strInstructions =>{
+    
+}
